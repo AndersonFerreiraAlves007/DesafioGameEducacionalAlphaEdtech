603d1ac0a72b3f11db8adfbf7649b6ca60ba7abe
AV-Felipe <aerogrado@hotmail.com>
2022-02-17 14:21:36 -0300

    Fix absolute path reference

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
      {
-        script: "server.js",
-        cwd: "/home/felipe/Insync/aerogrado@hotmail.com/OneDrive/01-estudos/ALPHA/aulas/hard/desafio-jogo/DesafioGameEducacionalAlphaEdtech/backend",
+        script: "backend/server.js",
+        cwd: "backend/",
         name: "Backend Game Educacional",
         watch: true
      },
      {
-        script: "server.js",
-        cwd: "/home/felipe/Insync/aerogrado@hotmail.com/OneDrive/01-estudos/ALPHA/aulas/hard/desafio-jogo/DesafioGameEducacionalAlphaEdtech/frontend",
+        script: "frontend/server.js",
+        cwd: "frontend/",
         name: "Frontend  Game Educacional",
         watch: true
      }
