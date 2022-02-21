acd8737886efcd4aff434af1df807e6425d79495
Sergio Pereira de Oliveira Júnior <sergio2pj@outlook.com>
2022-02-21 18:30:01 -0300

    Change in login screen

--- frontend/src/modules/login-register-script.js
+++ frontend/src/modules/login-register-script.js
@@ -2,19 +2,19 @@ import {serverConnection} from './server-communication.js';
 import { dadosGlobais } from './global-data.js'
 
 $(document).ready(function(){
-    $(".login").hide();
-    $(".register_li").addClass("active");
-    $(".login_li").click(function(){
-        $(this).addClass("active");
-        $(".register_li").removeClass("active");
-        $(".login").show();
-        $(".register").hide();
-    });
+    $(".register").hide();
+    $(".login_li").addClass("active");
     $(".register_li").click(function(){
         $(this).addClass("active");
         $(".login_li").removeClass("active");
         $(".register").show();
         $(".login").hide();
+    });
+    $(".login_li").click(function(){
+        $(this).addClass("active");
+        $(".register_li").removeClass("active");
+        $(".login").show();
+        $(".register").hide();
     });
     $("#btn_register").click(async function(){
         try{
