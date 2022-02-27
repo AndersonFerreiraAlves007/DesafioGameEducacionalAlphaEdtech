8bc829189860c0cdd56084b90148824cd78efa9f
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-02-27 18:32:34 -0300

    erro na rota utils/constants.js

--- frontend/server.js
+++ frontend/server.js
@@ -8,10 +8,6 @@ const PORT = 3334
 
 const app = express()
 
-app.use(express.static('src'))
-
-app.use('/login', express.static('src/pages/login-register.html'))
-
 app.get('/utils/constants.js', (req, res) => {
     res.append('Content-Type', 'application/javascript; charset=UTF-8');
     //res.header("Content-Type", "application/javascript")
@@ -26,6 +22,12 @@ app.get('/utils/constants.js', (req, res) => {
         `
     )
 })
+
+app.use(express.static('src'))
+
+app.use('/login', express.static('src/pages/login-register.html'))
+
+
 
 app.listen(PORT, () => {
     console.log(`linten => http://localhost:${PORT}`);
